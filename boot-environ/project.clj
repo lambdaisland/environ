c3023297446411a5f3f0a8241a65487194ccfbdd
Arne Brasseur <arne@arnebrasseur.net>
2023-11-13 13:55:28 +0100

    Remove boot-environ and lein-environ

--- boot-environ/project.clj
+++
@@ -1,6 +0,0 @@
-(defproject boot-environ "1.2.0"
-  :description "Boot task for Environ"
-  :url "https://github.com/weavejester/environ"
-  :scm {:dir ".."}
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"})
--- lein-environ/project.clj
+++
@@ -1,7 +0,0 @@
-(defproject lein-environ "1.2.0"
-  :description "Leiningen plugin for Environ"
-  :url "https://github.com/weavejester/environ"
-  :scm {:dir ".."}
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :eval-in-leiningen true)
